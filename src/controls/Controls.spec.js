b66ac6c1acf42ab5d87de84972b491a501ace0d2
Jenny Li <jli226@fordham.edu>
2019-08-20 08:52:04 -0700

    WIP

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,107 @@
 // Test away!
+import React from "react";
+import renderer from "react-test-renderer";
+import { render, fireEvent } from "@testing-library/react";
+import "jest-dom/extend-expect";
+
+import Controls from "./Controls";
+
+// shows the controls
+describe("<Controls />", () => {
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Controls />);
+
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
+
+// cannot be closed or opened if it is locked
+describe("<Controls />", () => {
+  it("cannot be closed or opened if it is locked", () => {
+    const { getByText } = render(<Controls locked={true} closed={true} />);
+
+    const openGate = getByText(/^Open Gate$/i);
+    expect(openGate.closest("button")).toHaveAttribute("disabled");
+  });
+});
+
+// provide button to toggle the closed state
+describe("<Controls />", () => {
+  it("provide button to toggle the closed state", () => {
+    const { getByText } = render(<Controls />);
+
+    const closeButton = getByText(/^Close Gate$/i);
+    expect(closeButton).toBeTrue;
+  });
+});
+
+// provide button to toggle the locked state
+describe("<Controls />", () => {
+  it("provide button to toggle the locked state", () => {
+    const { getByText } = render(<Controls />);
+
+    const closeButton = getByText(/^Close Gate$/i);
+    fireEvent.click(closeButton);
+
+    const lockButton = getByText(/^Lock Gate$/i);
+    expect(lockButton).toBeTrue;
+  });
+});
+
+// close button text changes to reflect the state the door will be in if clicked
+
+// tests if mock function is called on a click
+describe("<Controls />", () => {
+  it("tests if toggleClosed / mock function is called on a click", () => {
+    const mockFunction = jest.fn();
+    const { getByText } = render(<Controls toggleClosed={mockFunction} />);
+
+    const closeButton = getByText(/^Close Gate$/i);
+
+    fireEvent.click(closeButton);
+
+    expect(mockFunction).toHaveBeenCalled();
+  });
+});
+
+// tests if button text is Open Gate when closed is true
+describe("<Controls />", () => {
+  it("tests if button text is Open Gate when closed is true", () => {
+    const { getByText } = render(<Controls closed={true} />);
+
+    const openButton = getByText(/^Open Gate$/i);
+
+    expect(openButton).toHaveTextContent(/^Open Gate$/i);
+  });
+});
+
+// tests if button text is Close Gate when closed is false
+describe("<Controls />", () => {
+  it("tests if button text is Close Gate when closed is false", () => {
+    const { getByText } = render(<Controls closed={false} />);
+
+    const closeButton = getByText(/^Close Gate$/i);
+
+    expect(closeButton).toHaveTextContent(/^Close Gate$/i);
+  });
+});
+
+// the closed toggle button is disabled if the gate is locked
+describe("<Controls />", () => {
+  it("the closed toggle button is disabled if the gate is locked", () => {
+    const { getByText } = render(<Controls locked={true} />);
+
+    const closeButton = getByText(/^Close Gate$/i);
+    expect(closeButton).toHaveAttribute("disabled");
+  });
+});
+
+// the locked toggle button is disabled if the gate is open
+describe("<Controls />", () => {
+  it("the locked toggle button is disabled if the gate is open", () => {
+    const { getByText } = render(<Controls closed={false} />);
+
+    const lockButton = getByText(/^Lock Gate$/i);
+    expect(lockButton).toHaveAttribute("disabled");
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,105 @@
 // Test away!
+import React from "react";
+import renderer from "react-test-renderer";
+import { render } from "@testing-library/react";
+import "jest-dom/extend-expect";
+
+import Display from "./Display";
+
+// shows the display
+describe("<Display />", () => {
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Display />);
+
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
+
+// defaults to unlocked and open
+describe("<Display />", () => {
+  it("defaults to unlocked and open", () => {
+    const { getByText } = render(<Display />);
+
+    const unlocked = getByText(/^unlocked$/i);
+    const open = getByText(/open/i);
+
+    expect(unlocked).toBeTrue;
+    expect(open).toBeTrue;
+  });
+});
+
+// displays 'Closed' if the closed prop is true
+describe("<Display />", () => {
+  it("displays 'Closed' if the closed prop is true", () => {
+    const { getByText } = render(<Display closed={true} />);
+    const closed = getByText(/^Closed$/i);
+    expect(closed).toBeTrue;
+  });
+});
+
+// displays 'Open' if the closed prop is !true
+describe("<Display />", () => {
+  it("displays 'Open' if the closed prop is false", () => {
+    const { getByText } = render(<Display closed={!true} />);
+    const open = getByText(/^Open$/i);
+    expect(open).toBeTrue;
+  });
+});
+
+// displays 'Locked' if the locked prop is true
+describe("<Display />", () => {
+  it("displays 'Locked' if the locked prop is true", () => {
+    const { getByText } = render(<Display locked={true} />);
+    const locked = getByText(/^Locked$/i);
+    expect(locked).toBeTrue;
+  });
+});
+
+// displays 'Unlocked' if the locked prop is !true
+describe("<Display />", () => {
+  it("displays 'Unlocked' if the locked prop is true", () => {
+    const { getByText } = render(<Display locked={!true} />);
+    const unlocked = getByText(/^Unlocked$/i);
+    expect(unlocked).toBeTrue;
+  });
+});
+
+// when locked use the red-led class
+describe("<Display />", () => {
+  it("when locked use the red-led class", () => {
+    const { getByText } = render(<Display locked={true} />);
+
+    const lockedDiv = getByText(/^Locked$/i);
+    expect(lockedDiv).toHaveClass("led red-led");
+  });
+});
+
+// when closed use the red-led class
+describe("<Display />", () => {
+  it("when closed use the red-led class", () => {
+    const { getByText } = render(<Display closed={true} />);
+
+    const closedDiv = getByText(/^Closed$/i);
+    expect(closedDiv).toHaveClass("led red-led");
+  });
+});
+
+// when unlocked use the green-led class
+describe("<Display />", () => {
+  it("when unlocked use the green-led class", () => {
+    const { getByText } = render(<Display locked={false} />);
+
+    const unlockedDiv = getByText(/^Unlocked$/i);
+    expect(unlockedDiv).toHaveClass("led green-led");
+  });
+});
+
+// when open use the green-led class
+describe("<Display />", () => {
+  it("when open use the green-led class", () => {
+    const { getByText } = render(<Display closed={false} />);
+
+    const openDiv = getByText(/^Open$/i);
+    expect(openDiv).toHaveClass("led green-led");
+  });
+});
